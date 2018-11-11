ce633abe8be0eaf74eb956d82e2c6c76307e6faa
valerimartohan05112018 <valerimartohan05112018@gmail.com>
2018-11-11 14:52:23 +0100

    last mod

--- VP1/VP1.cpp
+++ VP1/VP1.cpp
@@ -23,13 +23,48 @@ int trovaMax(int iV[]) {
 	return iMax;
 }
 
+void ordinaVet(int iV[], int left, int right) {
+	int i = left, j = right;
+	int tmp;
+	int pivot = iV[(left + right) / 2];
+	/* partition */
+	while (i <= j) {
+		while (iV[i] < pivot)
+			i++;
+		while (iV[j] > pivot)
+			j--;
+		if (i <= j) {
+			tmp = iV[i];
+			iV[i] = iV[j];
+			iV[j] = tmp;
+			i++;
+			j--;
+		}
+	};
+	/* recursion */
+	if (left < j)
+		ordinaVet(iV, left, j);
+	if (i < right)
+		ordinaVet(iV, i, right);
+}
+
+void visualizzaV(int iV[]) {
+	cout << "Vettore ordinato V={ ";
+	for (int i = 0; i < 10; i++) {
+		cout << iV[i] << ", ";
+	}
+	cout << "}";
+}
+
 int main()
 {
 	int iV[10] = { -6,1,3,4,5,6,7,9,8,-3 };
 	cout << "Valore minimo: " << trovaMin(iV) << endl;
 	cout << "Valore massimo: " << trovaMax(iV) << endl;
-
+	ordinaVet(iV, 0, 9);
+	visualizzaV(iV);
 
 	system("PAUSE");
 	return 0;
 }
+
